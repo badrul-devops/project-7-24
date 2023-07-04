619836acca899d4b9528769144f7f0a1098eeaa9
Badrul_alam <badrulalam0123@gmail.com>
2023-07-04 12:47:15 +0600

    add code

--- ecommerce/service/models.py
+++ ecommerce/service/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
+    address = models.CharField(max_length=200)
+    product = models.CharField(max_length=100)
--- ecommerce/service/views.py
+++ ecommerce/service/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .form import UserForm
+from .models import User
 
-# Create your views here.
+def user_form(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('admin_portal')
+    else:
+        form = UserForm()
+    
+    return render(request, 'user_form.html', {'form': form})
+
+def admin_portal(request):
+    users = User.objects.all()
+    return render(request, 'admin_portal.html', {'users': users})
+
+
+def home(request):
+    return render(request, 'home.html')
